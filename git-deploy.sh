ce94018afb5c21e6306cca5b8ab500b817221bd8
Skyler Dowdy <skylerwebdev@gmail.com>
2020-03-11 11:20:08 -0400

    "$desc"

--- git-deploy.sh
+++ git-deploy.sh
@@ -4,5 +4,5 @@ read -r -p 'Commit message: ' desc  # prompt user for commit message
 read -r -p 'Would you like to add co authors?'
 git add .                           # track all files
 git add -u                          # track deletes
-git commit -m "'$desc"               # commit with message
+git commit -m '"$desc"'               # commit with message
 git push
