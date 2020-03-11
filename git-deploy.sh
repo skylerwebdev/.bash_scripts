16b93dda28cb53fa234a7b777bc774676dfb4948
Skyler Dowdy <skylerwebdev@gmail.com>
2020-03-11 12:05:52 -0400

    fix

--- git-deploy.sh
+++ git-deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 read -r -p 'Commit message: ' desc  # prompt user for commit message
-read -r -p 'Would you like to add co authors?'
 git add .                           # track all files
 git add -u                          # track deletes
-git commit -m '"$desc"'               # commit with message
+git commit -m "$desc"               # commit with message
 git push
